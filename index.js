77c5bae91c6bd4834bca15d22865a82a57dc3a0a
Xan <tsadiktalmudim@gmail.com>
2022-04-10 23:55:18 -0500

    attempted to debug for the past two hours. Need sleep

--- index.js
+++ index.js
@@ -1,136 +1,145 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = "../utils/generateMarkdown.js";
+const { generateMarkdown, renderLicenseBadge, renderLicenseSection } = require('./utils/generateMarkdown.js');
 const { title } = require("process");
 
 // TODO: Create an array of questions for user input
-class promptUser {
-  constructor() {
-    return inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "title",
-          message: "What's the name of your project? (required)",
-          validate: (projectInput) => {
-            if (projectInput) {
-              return true;
-            } else {
-              console.log("Please enter your project name!");
-              return false;
-            }
-          },
+const promptUser =
+//   return inquirer
+//     .prompt([
+    [
+      {
+        type: "input",
+        name: "title",
+        message: "What's the name of your project? (required)",
+        validate: (projectInput) => {
+          if (projectInput) {
+            return true;
+          } else {
+            console.log("Please enter your project name!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "username",
-          message: "What is your GitHub username?",
-          validate: (usernameInput) => {
-            if (usernameInput) {
-              return true;
-            } else {
-              console.log("Please enter your username!");
-              return false;
-            }
-          },
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username? (required)",
+        validate: (usernameInput) => {
+          if (usernameInput) {
+            return true;
+          } else {
+            console.log("Please enter your username!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "gitLink",
-          message: "Please enter your GitHub account URL",
-          validate: (linkInput) => {
-            if (linkInput) {
-              return true;
-            } else {
-              console.log("Please enter a description!");
-              return false;
-            }
-          },
+      },
+      {
+        type: "input",
+        name: "gitLink",
+        message: "Please enter your GitHub account URL (required)",
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log("Please enter a description!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "email",
-          message:
-            "Please provide your email address for any questions related to the project and contribution",
-          validate: (emailInput) => {
-            if (emailInput) {
-              return true;
-            } else {
-              console.log("Please enter a description!");
-              return false;
-            }
-          },
+      },
+      {
+        type: "input",
+        name: "email",
+        message:
+          "Please provide your email address for any questions related to the project and contribution (required)",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter a description!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "description",
-          message: "Provide a description about your project (required)",
-          validate: (descInput) => {
-            if (descInput) {
-              return true;
-            } else {
-              console.log("Please enter a description!");
-              return false;
-            }
-          },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a description about your project (required)",
+        validate: (descInput) => {
+          if (descInput) {
+            return true;
+          } else {
+            console.log("Please enter a description!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "usage",
-          message: "Please describe how to use this application. (required)",
-          validate: (usageInput) => {
-            if (usageInput) {
-              return true;
-            } else {
-              console.log("Please enter a usage description!");
-              return false;
-            }
-          },
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Please describe how to use this application. (required)",
+        validate: (usageInput) => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log("Please enter a usage description!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "installation",
-          message: "Please provide installation instructions for your project",
-          validate: (installInput) => {
-            if (!installInput) {
-              this.message = "No instructions necessary.";
-            }
-          },
-        },
-        {
-          type: "input",
-          name: "contributors",
-          message:
-            "Please provide the GitHub links for contributors to your project here",
-        },
-        {
-          type: "confirm",
-          name: "testAsk",
-          message: "Would you like to add tests for your code?",
-          default: false,
-          validate,
-        },
-        {
-          type: "confirm",
-          name: "licenseConfirm",
-          message: "Would you like to add a license?",
-          default: false,
-        },
-      ])
-      .then((licenseData) => {
-        if (licenseData.licenseConfirm) {
-          renderLicenseLink(licenseData);
-        }
-        return [""];
-      });
-  }
-}
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Please provide installation instructions for your project",
+      },
+      {
+        type: "input",
+        name: "contributors",
+        message:
+          "Please provide the GitHub links for contributors to your project here",
+      },
+      {
+        type: "confirm",
+        name: "testAsk",
+        message: "Would you like to add tests for your code?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "licenseConfirm",
+        message: "Would you like to add a license?",
+        default: false,
+      },
+    ];
+    // .then((licenseData) => {
+    //   if (licenseData.licenseConfirm) {
+    //     renderLicenseSection(licenseData);
+    //   }
+    //   return [""];
+    // })
+    // .then(data => {
+    //     console.log(data)
+    //     fs.writeFile('./dist/README.md', generateMarkdown(data), (err) => {
+    //         if (err) {
+    //             console.log(err)
+    //         } else {
+    //             console.log('success')
+    //         }
+        // })
+    // })
+// };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    inquirer.prompt(promptUser).then((data) => {
+        console.log(data);
+        fs.writeFile('GeneratedREADME.md', generateMarkdown(data), (err) => err ? console.log('ERROR!') : console.log('README has been generated!'))
+    })
+}
 
 // Function call to initialize app
 init();
+// promptUser();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,69 @@
-const fs = require(fs);
+const inquirer = require("inquirer");
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-class renderLicenseSection {
-  constructor(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "licenseName",
+      message: "What license are you using for this project?",
+    },
+    {
+      type: "checkbox",
+      name: "color",
+      choices: ["Brightgreen", "Green", "Blue", "Lightgrey"],
+    },
+  ])
+  .then(renderLicenseLink, renderLicenseBadge)
+}
 
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
-  renderLicenseLink(license) {}
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(data) {
+  this.license = license;
+  return `#${data.licenseName}
+  Copyright (c) ${new Date().getFullYear()}\n
+  For more information about licensing, please visit [choosealicense.com].(https://choosealicense.com/)
+  `
+}
 
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
-  renderLicenseBadge(license) {}
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseBadge(data) {
+  license = `https://img.shields.io/badge/license-${data.licenseName}-${data.color}`;
 }
 
 // TODO: Create a function to generate markdown for README
-class generateMarkdown {
-  constructor(data) {
-    return `# ${data.title}
-
+function generateMarkdown(data) {
+  return `## ${data.title}
+  ${renderLicenseBadge(data.license)}
+  # Description
+  ${data.description}
+  # Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Questions](#questions)
+  
+  # Installation
+  ${data.installation}
+  # Usage
+  ${data.usage}
+  ${renderLicenseSection(data.licenseConfirm)}
+  ${renderLicenseLink(data.license)}
+  # Contribution
+  ${data.contributors}
+  # Questions
+  If you have any question you can contact me here: 
+  *[${data.username}](${data.gitLink})
+  *email: ${data.email}
 `;
-  }
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown, renderLicenseBadge, renderLicenseSection };
